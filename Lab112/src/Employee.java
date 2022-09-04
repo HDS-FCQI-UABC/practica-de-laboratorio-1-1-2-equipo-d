a2b519d8ed048c7cf765bc88afb459262f0fbe7d
secretarybird97 <81328194+secretarybird97@users.noreply.github.com>
2022-09-03 17:41:26 -0700

    Agregue el modulo LAB112 y subi Employee and Resource

---
+++ Lab112/src/Employee.java
@@ -0,0 +1,23 @@
+public class Employee {
+
+    private int name;
+
+    public Employee() {
+        // TODO - implement Employee.Employee
+        throw new UnsupportedOperationException();
+    }
+
+    public void getName() {
+        // TODO - implement Employee.getName
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     *
+     * @param name
+     */
+    public void setName(int name) {
+        this.name = name;
+    }
+
+}
