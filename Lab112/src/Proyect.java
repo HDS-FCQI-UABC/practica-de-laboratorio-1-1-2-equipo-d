0e5fe20983d1ae2e938a9bc3217dc66a1feda8b3
Zwueyn <sebastian.valenzuela@uabc.edu.mx>
2022-09-03 18:14:03 -0700

    Clase principal (Proyect)

---
+++ Lab112/src/Proyect.java
@@ -0,0 +1,65 @@
+public class Proyect {
+
+    private int name;
+    private int theResources;
+    private int theEmployees;
+    private int theWorkBreakdownStructure;
+
+    public void getName() {
+        // TODO - implement Proyect.getName
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     *
+     * @param name
+     */
+    public void setName(int name) {
+        this.name = name;
+    }
+
+    public void getTheResources() {
+        // TODO - implement Proyect.getTheResources
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     *
+     * @param theResources
+     */
+    public void setTheResources(int theResources) {
+        this.theResources = theResources;
+    }
+
+    public void getTheEmployees() {
+        // TODO - implement Proyect.getTheEmployees
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     *
+     * @param theEmployees
+     */
+    public void setTheEmployees(int theEmployees) {
+        this.theEmployees = theEmployees;
+    }
+
+    public void getTheWorkBreakdownStructure() {
+        // TODO - implement Proyect.getTheWorkBreakdownStructure
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     *
+     * @param theWorkBreakdownStructure
+     */
+    public void setTheWorkBreakdownStructure(int theWorkBreakdownStructure) {
+        this.theWorkBreakdownStructure = theWorkBreakdownStructure;
+    }
+
+    public Proyect() {
+        // TODO - implement Proyect.Proyect
+        throw new UnsupportedOperationException();
+    }
+
+}
