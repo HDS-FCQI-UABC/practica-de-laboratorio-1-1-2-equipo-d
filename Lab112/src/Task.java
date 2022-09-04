63523292793dacc77c60a6bebc1d57d585ff5058
barribam <barribam@uabc.edu.mx>
2022-09-03 18:06:08 -0700

    clase Task

---
+++ Lab112/src/Task.java
@@ -0,0 +1,51 @@
+public class Task {
+
+    private int name;
+    private int theEmployees;
+    private int theResourcers;
+
+    public Task() {
+        // TODO - implement Task.Task
+        throw new UnsupportedOperationException();
+    }
+
+    public void getName() {
+        // TODO - implement Task.getName
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     *
+     * @param name
+     */
+    public void setName(int name) {
+        this.name = name;
+    }
+
+    public void getTheEmployees() {
+        // TODO - implement Task.getTheEmployees
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     *
+     * @param theEmployees
+     */
+    public void setTheEmployees(int theEmployees) {
+        this.theEmployees = theEmployees;
+    }
+
+    public void getTheResourcers() {
+        // TODO - implement Task.getTheResourcers
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     *
+     * @param theResourcers
+     */
+    public void setTheResourcers(int theResourcers) {
+        this.theResourcers = theResourcers;
+    }
+
+}
